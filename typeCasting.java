768191cd3d76d9f99d10fc03e7a836490284e980
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-29 23:52:14 +0530

    float typecasting added

--- typeCasting.java
+++ typeCasting.java
@@ -5,7 +5,10 @@ class typeCasting{
         byte c = (byte)b;
         int d = 258;
         byte e = (byte)d;
+        float f = 9.6f;
+        int g = (int)f;
         System.out.println(c);
         System.out.println(e);// 258 % 256 = 2
+        System.out.println(g);// 9.6 -> 9
     }
 }
