138fc0503e3eed3d7c73cbc21a36837661bac5c9
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-29 23:25:50 +0530

    data types all

--- addNum.java
+++ addNum.java
@@ -4,8 +4,23 @@ class addNum
        int a = 3;
        int b = 5;
        int sum = a + b;
+       float c = 5.6f;
+       byte d = 127;
+       short e = 558;
+       long f = 1234567890L;
+       double g = 3.14159;
+         char h = 'A';
+         boolean i = true;
        System.out.println(a + "+" + b + "=" + sum);
        System.out.println(a+b);
        System.out.println(sum);
+       System.out.println(c+sum);
+       System.out.println(d);
+       System.out.println(e);
+       System.out.println(f);
+       System.out.println(g);
+       System.out.println(h);
+         System.out.println(i);
+         
     }
 }
