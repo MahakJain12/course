095eba0899be197de0fd961c9ef1b113c4027cc3
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-30 00:17:48 +0530

    arithmetic operations

---
+++ arithmeticOps.java
@@ -0,0 +1,25 @@
+class arithmeticOps{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int result1= a+b;
+        int result2= a-b;
+        int result3= a*b;
+        int result4= a/b;
+        int result5= a%b;
+        // int result6= a++;
+        int result7= ++a;
+        int result8= a--;
+        int result9= --a;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);            
+        System.out.println(result5);
+        // System.out.println(result6);
+        System.out.println(result7);
+        System.out.println(result8);
+        System.out.println(result9);
+    
+    }
+}
