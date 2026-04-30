5d1489a2de61b59d5acfccbe40ee62be0de27002
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-30 22:54:09 +0530

    switch statement

---
+++ switchStatement.java
@@ -0,0 +1,18 @@
+class switchStatement{
+    public static void main(String [] args){
+    int n = 5;
+
+    switch(n){
+        case 1:
+            System.out.println("Monday");
+        case 2:
+            System.out.println("Wednesday");
+        case 3:
+            System.out.println("Thursday");
+        case 4:
+            System.out.println("Thursday");
+        case 5:
+            System.out.println("Friday");        
+       }
+    }
+}
