cf58b24b00492d207dcfa0409110fc80a5b732de
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-30 23:28:48 +0530

    do while loop

---
+++ doWhileLoop.java
@@ -0,0 +1,9 @@
+class doWhile{
+    public static void main(String[] args){
+        int n = 5;
+        do{
+            System.out.println("Hi");
+            n++;
+        } while(n<=4);
+    }
+}
