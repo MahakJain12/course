c1154efe63738a76ac667552ead6f4eb60aa017c
Mahak Jain <mahakjain@Mahaks-MacBook-Pro.local>
2026-04-30 22:57:23 +0530

    added break and default

--- switchStatement.java
+++ switchStatement.java
@@ -5,14 +5,22 @@ class switchStatement{
     switch(n){
         case 1:
             System.out.println("Monday");
+            break;
         case 2:
             System.out.println("Wednesday");
+            break;
         case 3:
             System.out.println("Thursday");
+            break;
         case 4:
             System.out.println("Thursday");
+            break;
         case 5:
-            System.out.println("Friday");        
+            System.out.println("Friday");
+            break;
+         default:
+            System.out.println("Holiday");
+            break;         
        }
     }
 }
